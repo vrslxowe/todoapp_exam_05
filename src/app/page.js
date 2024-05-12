cbfd7fe4a683b054e4807c2b3a0c024106c3082f
vrslxowe <vrslxowe@gmail.com>
2024-05-13 02:56:04 +0900

    수정2

--- src/app/page.js
+++ src/app/page.js
@@ -170,9 +170,9 @@ const TodoListItem = ({ todo, index, openDrawer, removeTodo }) => {
             />
           </div>
           <div className="tw-rounded-[10px] tw-shadow tw-flex tw-text-[14px] tw-min-h-[80px]">
-            <Checkbox />
             <div className="tw-bg-[#dcdcdc] tw-w-[2px] tw-h-[60px] tw-self-center"></div>
             <div className="tw-bg-blue-300 tw-flex tw-items-center tw-p-3 tw-flex-grow hover:tw-text-[--mui-color-primary-main] tw-whitespace-pre-wrap tw-leading-relaxed tw-break-words">
+              <Checkbox />
               {todo.content}
             </div>
             <Button
